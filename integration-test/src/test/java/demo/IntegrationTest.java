c36ba0e40f3ca4c276dd336aa9b50eea5c1e55be
Dariusz Lorenc <dariusz.lorenc@gmail.com>
2016-09-11 17:17:12 +1000

    Use master service's uri as rest-assured base uri

--- integration-test/src/test/java/demo/IntegrationTest.java
+++ integration-test/src/test/java/demo/IntegrationTest.java
@@ -3,10 +3,7 @@ package demo;
 import com.jayway.restassured.RestAssured;
 import com.palantir.docker.compose.DockerComposeRule;
 import com.palantir.docker.compose.connection.DockerPort;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.function.Function;
 
@@ -17,22 +14,26 @@ import static org.hamcrest.Matchers.isA;
 
 public class IntegrationTest {
 
+    private static final int INTERNAL_PORT = 8080;
+
     private static final Function<DockerPort, String> TO_EXTERNAL_URI =
             (port) -> port.inFormat("http://$HOST:$EXTERNAL_PORT");
 
     @Rule
     public DockerComposeRule docker = DockerComposeRule.builder()
             .file("../docker-compose.yml")
-            .waitingForService("greeting-service", toRespondOverHttp(8080, TO_EXTERNAL_URI))
-            .waitingForService("counter-service", toRespondOverHttp(8080, TO_EXTERNAL_URI))
-            .waitingForService("master-service", toRespondOverHttp(8080, TO_EXTERNAL_URI))
+            .waitingForService("greeting-service", toRespondOverHttp(INTERNAL_PORT, TO_EXTERNAL_URI))
+            .waitingForService("counter-service", toRespondOverHttp(INTERNAL_PORT, TO_EXTERNAL_URI))
+            .waitingForService("master-service", toRespondOverHttp(INTERNAL_PORT, TO_EXTERNAL_URI))
             .saveLogsTo("build/docker-logs")
             .build();
 
     @Before
     public void setUp() throws Exception {
         docker.dockerCompose().up();
-        RestAssured.port = 8083;
+
+        DockerPort masterService = docker.containers().container("master-service").port(INTERNAL_PORT);
+        RestAssured.baseURI = TO_EXTERNAL_URI.apply(masterService);
     }
 
     @After
